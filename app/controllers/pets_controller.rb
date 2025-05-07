6052f3537641c53bd9bb485dfc53cae45f246bbb
Rita Huang <yuhruhgrape@gmail.com>
2025-05-07 22:32:25 +0800

    add @pet.user in pet_controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -22,6 +22,7 @@ class PetsController < ApplicationController
   # POST /pets or /pets.json
   def create
     @pet = Pet.new(pet_params)
+    @pet.user = current_user
 
     respond_to do |format|
       if @pet.save
