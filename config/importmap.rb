88d34b44a04541415f44a8ede88d5adb74e56f23
Rita Huang <yuhruhgrape@gmail.com>
2025-05-12 12:29:52 +0800

    add set time zone explicitly for users function

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "bootstrap.bundle.min.js"
-pin "chartkick", to: "chartkick.js"
-pin "Chart.bundle", to: "Chart.bundle.js"
+pin "react" # @19.1.0
+pin "react-dom" # @19.1.0
