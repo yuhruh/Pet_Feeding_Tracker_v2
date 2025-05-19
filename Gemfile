75b6298d6afc9d225350147ab5d800a03f7ac44f
Rita Huang <yuhruhgrape@gmail.com>
2025-05-19 20:13:44 +0800

    check timezone and time

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem "chartkick"
 gem "groupdate"
 gem 'byebug', '~> 11.1', '>= 11.1.3'
 
+gem 'tzinfo'
+
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 gem "bcrypt", "~> 3.1.7"
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,6 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  # def current_pet 
-  #   Pet.find(@pet.id)
-  # end
-
   def logged_in?
     !!current_user
   end
