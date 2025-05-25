685c25410464a43fd713149cc5e79d7fb8abfc82
Rita Huang <yuhruhgrape@gmail.com>
2025-05-25 21:50:29 +0800

    add weight remind in the form

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,7 +2,6 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_many :trackers, dependent: :destroy
   validates :pet_name, presence: true, 
-                      uniqueness: true, 
                       length: { minimum: 2, maximum: 25 }
   validates :weight, numericality: {message: "The weight should be greather than 0 and less than 100"}, 
         comparison: { less_than_or_equal_to: 100, greater_than: 0
