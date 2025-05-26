1677e5c5f7aeadee6c604c99657539deb2a94f89
Rita Huang <yuhruhgrape@gmail.com>
2025-05-26 22:34:45 +0800

    change route after sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:session][:name])
     if user && user.authenticate_password(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = "Logged in successfully."
-      redirect_to '/favorites'
+      flash[:notice] = "Welcome to Cat Feeding Tracker."
+      redirect_to '/pets'
     else
       flash.now[:alert] = "Invalid usesrname or Password"
       render :new, status: :unprocessable_entity
