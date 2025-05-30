c38ffda9e42591cb30176547b25caf0e00298bd7
Rita Huang <yuhruhgrape@gmail.com>
2025-05-30 23:14:43 +0800

    change about to doc

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "/home", to: "pages#home"
-  get "/about", to: "pages#about"
+  get "/doc", to: "pages#doc"
   get "/started", to:"pages#started"
   get "signup", to: 'users#new'
   get 'login', to: 'sessions#new'
