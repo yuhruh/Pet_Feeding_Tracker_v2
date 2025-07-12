8e40e3e32e22bcd1afd2687636898d99530dfe3e
Rita Huang <yuhruhgrape@gmail.com>
2025-07-12 20:36:03 +0800

    fix date and time in timezone issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   helper_method :current_user, :set_current_time, :set_current_date, :logged_in?
+  before_action :set_current_date, :set_current_time
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
