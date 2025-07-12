9db9ebf7a0a5d8c2fd8002de265c236602642986
Rita Huang <yuhruhgrape@gmail.com>
2025-07-12 14:21:14 +0800

    comment out config.timezone

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module PetFeedingTrackerV2
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-    config.time_zone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    # config.time_zone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
     # Configuration for the application, engines, and railties goes here.
     #
