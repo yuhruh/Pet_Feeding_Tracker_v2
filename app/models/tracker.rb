a798d0653e01ab00f051f2ed0a21c3904661126d
Rita Huang <yuhruhgrape@gmail.com>
2025-07-12 15:08:27 +0800

    comment out attributes on tracker.rb

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -3,7 +3,7 @@ class Tracker < ApplicationRecord
   before_save { self.brand = brand.downcase }
   before_save { self.description = description.downcase }
   # before_save { self.date = set_current_date}
-  attribute :date, :datetime, default: ->{ Time.current }
+  # attribute :date, :datetime, default: ->{ Time.current }
   validates :food_type, presence: true
   validates :brand, presence: true, length: {minimum: 1, maximum: 50}
   validates :description, presence: true, length: {minimum: 2, maximum: 100}
