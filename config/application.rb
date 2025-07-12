cf3cc4c9f530e75044a76af7e521dc2dfe203315
Rita Huang <yuhruhgrape@gmail.com>
2025-07-12 12:33:34 +0800

    add config time_zone in application.rb in config

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module PetFeedingTrackerV2
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.time_zone = current_user.time_zone
 
     # Configuration for the application, engines, and railties goes here.
     #
