054b552443ec52e3073e4be58639174cf353bd1e
Rita Huang <yuhruhgrape@gmail.com>
2025-07-12 19:14:20 +0800

    fix time and date in users time zone

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -2,8 +2,7 @@ class Tracker < ApplicationRecord
   belongs_to :pet
   before_save { self.brand = brand.downcase }
   before_save { self.description = description.downcase }
-  # before_save { self.date = set_current_date}
-  # attribute :date, :datetime, default: ->{ Time.current }
+  # attribute :date, :datetime, default: ->{ Time.current } #casting
   validates :food_type, presence: true
   validates :brand, presence: true, length: {minimum: 1, maximum: 50}
   validates :description, presence: true, length: {minimum: 2, maximum: 100}
